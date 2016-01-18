b2e883fcbccf356b2f3abbdaa8a6cac9b46defd8
Anton Stupak <s2pak.anton@gmail.com>
2016-01-18 11:38:02 +0800

    Restore set_staff method.

--- openedx/core/djangoapps/labster/course/utils.py
+++ openedx/core/djangoapps/labster/course/utils.py
@@ -1,9 +1,25 @@
 """ Labster Course utils. """
 import logging
 
+from django.contrib.auth.models import User
+from contentstore.utils import add_instructor, remove_all_instructors
 from openedx.core.djangoapps.labster.exceptions import LtiPassportError
 
 log = logging.getLogger(__name__)
+
+
+def set_staff(course_key, emails):
+    """
+    Sets course staff.
+    """
+    remove_all_instructors(course_key)
+    for email in emails:
+        try:
+            user = User.objects.get(email=email)
+            add_instructor(course_key, user, user)
+        except User.DoesNotExist:
+            log.info('User with email %s does not exist', email)
+
 
 class LtiPassport(object):
     """
