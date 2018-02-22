6659de73f8bbf0328c1e9c60e56f7f2f020760d7
Vladas Tamoshaitis <amd.vladas@gmail.com>
2018-02-23 01:07:42 +0200

    raise exception if region is not configured

--- openedx/core/djangoapps/labster/regions.py
+++ openedx/core/djangoapps/labster/regions.py
@@ -3,6 +3,7 @@ Region module.
 Can contain region specific logic or functionality.
 """
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 
@@ -13,7 +14,13 @@ class Region(object):
     def __init__(self, request):
         regions = configuration_helpers.get_value('REGIONS', settings.REGIONS)
         # List of country codes: https://dev.maxmind.com/geoip/legacy/codes/iso3166/
-        current_region = request.session.get('country_code')
-        params = regions[current_region] if current_region in regions.keys() else {}
-        for key, val in params.items():
-            setattr(self, key, val)
+        country_code = request.session.get('country_code')
+        current_region = country_code
+        if current_region in regions.keys():
+            params = regions[current_region]
+            for key, val in params.items():
+                setattr(self, key, val)
+        else:
+            raise ImproperlyConfigured(
+                "Region object can not be created from country code `%s`" % country_code
+            )
